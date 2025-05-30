67f1c2938cd3bd5ba480f6a83e705f4a0e122e80
yooso <yooso0731@gmail.com>
2025-05-30 10:26:39 +0900

    Refactor: agent 코드 리팩토리

---
+++ agents/main_chatbot/config.py
@@ -0,0 +1,2 @@
+MODEL_NAME="gpt-40-mini"
+TEMPERATURE=0
---
+++ agents/main_chatbot/role_model_agent.py
@@ -0,0 +1,52 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+from prompt import roleModel_prompt
+from config import MODEL_NAME, TEMPERATURE
+
+
+
+# 환경변수 로드
+load_dotenv()
+
+# LLM 초기화
+api_key = os.getenv("OPENAI_API_KEY")
+if not api_key:
+    raise ValueError("OPENAI_API_KEY 환경변수가 설정되지 않았습니다.")
+
+llm = ChatOpenAI(model=MODEL_NAME, api_key=api_key, temperature=TEMPERATURE)
+
+roleModel_chain = roleModel_prompt | llm | StrOutputParser()
+
+def roleModel_invoke(state: dict, config=None) -> dict:
+    """roleModel Chain 실행 함수"""
+
+    messages_Text = "\n".join([
+        msg.content for msg in state.get("messages", [])
+        if hasattr(msg, 'content')
+    ])
+
+    # 사용자 정보
+    information = state.get("information", "")
+
+    result = roleModel_chain.invoke({
+        "messages": messages_text,
+        "information": information
+    })
+
+    new_messages = list(state.get("messages", []))
+    new_messages.append(AIMessage(content=result, name="RoleModel"))
+
+    return {
+        **state,
+        "messages": new_messages
+    }
+
+def roleModel_node(state):
+    """RoleModel 노드 함수"""
+    return roleModel_invoke(state)
