97e03e0d59b2e6874dbf2c9b548ab21bd1670b34
ilikewhale <rla0576@naver.com>
2025-05-30 09:50:29 +0900

    Feat:임시 api주소 생성

--- api/fastserver.py
+++ api/fastserver.py
@@ -1,18 +1,25 @@
 from fastapi import FastAPI
+import uvicorn
 
-app = FastAPI(docs_url="/apis/docs",openapi_url="/apis/openapi.json")
+app = FastAPI(docs_url="/apis/docs", openapi_url="/apis/openapi.json")
 
+@app.post("/apis/v1/career-path")
+def career_path():
+    return "커리어추천: cloud 강의 듣기"
 
-@app.get("/apis/test")
-async def read_root():
-    return {"Hello": "World"}
+@app.post("/apis/v1/rolemodel")
+def rolemodel():
+    return "rolemodel: cloud 마스터"
+
+@app.post("/apis/v1/career-summary")
+def career_summary():
+    return "career-summary: 당신은 cloud 최고 전문가 김채연매니저입니다>_<"
 
 if __name__ == "__main__":
     import argparse
-    import uvicorn
-
+    
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', type=int, default=8000)
     args = parser.parse_args()
-
+    
     uvicorn.run(app, host="0.0.0.0", port=args.port)
