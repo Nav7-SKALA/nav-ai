cf9474dd854a98d9e535f0f37797a0c77c1ae12e
ilikewhale <rla0576@naver.com>
2025-06-04 20:03:13 +0900

    Feat:HTTPException 추가

--- api/fastserver.py
+++ api/fastserver.py
@@ -1,14 +1,20 @@
 import sys
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
 from pydantic import BaseModel
+from openai import (APIError,
+                        AuthenticationError,
+                        APITimeoutError,
+                        RateLimitError,
+                        APIConnectionError
+                        )
 
 from config import BASE_DIR, AGENT_ROOT, AGENT_DIR
 
 sys.path.append(BASE_DIR)
 sys.path.append(AGENT_ROOT)
 sys.path.append(AGENT_DIR['main_chatbot'])
-from main_chatbot.graph import run_main_chatbot
+from main_chatbot.graph import create_workflow, create_initial_state, create_response
 
 app = FastAPI(docs_url="/apis/docs", openapi_url="/apis/openapi.json")
 
@@ -20,14 +26,52 @@ app = FastAPI(docs_url="/apis/docs", openapi_url="/apis/openapi.json")
 class career_path_request(BaseModel):
     user_query: str
     user_id: str
-
+    
 @app.post("/apis/v1/career-path")
 def career_path(request: career_path_request):
     """메인 챗봇 API"""
     
-    result = run_main_chatbot(request.user_query)
-    
-    return result
+    #result = run_main_chatbot(request.user_query)
+    graph = create_workflow()
+    initial_state = create_initial_state(request.user_query)
+
+    try:
+        result = graph.invoke(initial_state)
+        result_content = create_response(result)
+        
+        return result_content
+
+    except APITimeoutError as e:
+        raise HTTPException(
+            status_code=504,
+            detail=f"요청 제한 시간 초과: {e.__class__.__name__}"
+        )
+    except RateLimitError as e:
+        raise HTTPException(
+            status_code=429,
+            detail=f"API 요청 한도 초과: {e.__class__.__name__}"
+        )
+    except AuthenticationError as e:
+        raise HTTPException(
+            status_code=401,
+            detail=f"인증 오류: {e.__class__.__name__}"
+        )
+    except APIConnectionError as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"OpenAI 연결 오류: {e.__class__.__name__}"
+        )
+    except APIError as e:
+        # 그 외 OpenAI 관련 에러: 서버 오류(500)
+        raise HTTPException(
+            status_code=500,
+            detail=f"OpenAI API 오류: {e.__class__.__name__}"
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"서버 내부 오류: {str(e)}"
+        )
 
 
 @app.post("/apis/v1/rolemodel")
