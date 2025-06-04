a1068fff46e20e32b2365102c02c1682ca470883
ilikewhale <rla0576@naver.com>
2025-06-04 16:20:20 +0900

    Feat:config 추가

---
+++ agents/main_chatbot/config.py
@@ -0,0 +1,14 @@
+import os
+
+# 기본 경로 설정
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+
+AGENT_ROOT = os.path.join(BASE_DIR, "agents")
+AGENT_DIR = {'main_chatbot': os.path.join(AGENT_ROOT, "main_chatbot"),
+             'persona_chat': os.path.join(AGENT_ROOT, "persona_chat"),
+             'summary': os.path.join(AGENT_ROOT, "summary"),
+            }
+
+# 모델 파라미터
+MODEL_NAME="gpt-4o-mini"
+TEMPERATURE=0
