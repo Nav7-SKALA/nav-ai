af36822e75261af3b73b490cfed5f5e373e5deff
yooso <yooso0731@gmail.com>
2025-07-02 09:30:11 +0900

    Feat/경력 증진 출력에 회사 방향성 주입

--- db/postgres.py
+++ db/postgres.py
@@ -23,3 +23,24 @@ def get_career_summary(member_id: str):
         return "None"
     return result[0]
 
+def get_company_direction():
+    # PostgreSQL 연결
+    conn = psycopg2.connect(os.getenv("POSTGRES_URL"))
+    # 커서 생성
+    cur = conn.cursor()
+    try:    
+        # 최근 행 출력 쿼리 실행
+        cur.execute("""
+            SELECT prompt 
+            FROM public.direction 
+            ORDER BY created_at DESC 
+            LIMIT 1;
+        """)
+        result = cur.fetchone()
+    except:
+        # 하드코딩 버전
+        result = ['모든 업무와 프로젝트에 AI를 기본 적용할 줄 아는 AI 기본 역량을 갖춘 사내 구성원']
+    # 연결 종료
+    cur.close()
+    conn.close()
+    return result[0]
--- vector_store/chroma_search.py
+++ vector_store/chroma_search.py
@@ -6,7 +6,9 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
-from chroma_client import get_chroma_client
+import sys
+sys.path.append('..') 
+from vector_store.chroma_client import get_chroma_client
 
 from dotenv import load_dotenv
 
