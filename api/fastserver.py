90215a535ede77b6be93a75dd2869263a056ff4c
ilikewhale <rla0576@naver.com>
2025-07-03 14:52:05 +0900

    Feat:fastapi 포트번호 수정

--- api/fastserver.py
+++ api/fastserver.py
@@ -376,7 +376,7 @@ if __name__ == "__main__":
     import argparse
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('--port', type=int, default=8001)
+    parser.add_argument('--port', type=int, default=8000)
     args = parser.parse_args()
     
     uvicorn.run(app, host="0.0.0.0", port=args.port)
